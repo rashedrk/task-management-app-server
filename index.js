4df34c8a777ddde612f7c3e092f5aa4bdab93fc1
rashedrk <rashedrk001@gmail.com>
2023-06-04 21:28:02 +0600

    delete a task form db

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-require('dotenv').config()
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 // middle Ware
 app.use(cors());
