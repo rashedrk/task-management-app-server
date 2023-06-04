e9dc2c973127496cf2c46b05f447229dc46648fb
rashedrk <rashedrk001@gmail.com>
2023-06-04 21:22:04 +0600

    add new task to database

--- index.js
+++ index.js
@@ -21,12 +21,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const tasksCollection = client.db('taskManagement').collection('tasks');
+
+        //Create
+        //add new task to database
+        app.post('/tasks',async(req,res) => {
+            const newTask = req.body;
+            const result = await tasksCollection.insertOne(newTask);
+            res.send(result);
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
